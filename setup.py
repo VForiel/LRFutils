687e45c78b65ce00656b089920e658a0f4f9a7c3
Vincent (Leirof) <vince.lrf@gmail.com>
2022-06-28 15:05:05 +0200

    Test

--- setup.py
+++ setup.py
@@ -11,3 +11,27 @@ setup(
     description="Just a custom library to share with some colleagues.",
     long_description=open('README.md').read(),
 )
+
+setup(
+    name="LRFutils",
+    version = "0.0.5",
+    description = 'Just a custom library to share with some colleagues. Use it at your own risks.',
+    author = 'ZRunner',
+    author_email = 'vince.lrf@gmail.com',
+    url = 'https://github.com/LeiRoF/Utils',
+    packages=['LRFutils'],
+    install_requires=[
+        'gitpython',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'regex'
+        ],
+    python_requires='>3.9.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers'
+    ]
+)
