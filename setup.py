fa824ee46e2332858b7cab190115c52569a09299
Vincent (Leirof) <vince.lrf@gmail.com>
2022-06-28 15:05:20 +0200

    oops

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='LRFutils',
-    url='https://github.com/LeiRoF/Utils',
-    author='Vincent Foriel',
-    author_email='vince.lrf@gmail.com',
-    install_requires=['numpy'],
-    version='0.4',
-    description="Just a custom library to share with some colleagues.",
-    long_description=open('README.md').read(),
-)
-
-setup(
     name="LRFutils",
     version = "0.0.5",
     description = 'Just a custom library to share with some colleagues. Use it at your own risks.',
