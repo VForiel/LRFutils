0a2108113407150a8ff41282b722c27656bcb8e2
leirof <vince.lrf@gmail.com>
2022-10-22 11:32:56 +0200

    Updated pypi description

--- setup.py
+++ setup.py
@@ -4,14 +4,22 @@ requirements = []
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name="LRFutils",
-    version = "0.0.13",
+    version = "0.0.14",
     description = 'Just a custom library to share with some colleagues. Use it at your own risks.',
     author = 'Leirof',
     author_email = 'vince.lrf@gmail.com',
     url = 'https://github.com/LeiRoF/Utils',
     readme = "README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['LRFutils'],
     install_requires=requirements,
     python_requires='>=3.10.0',
